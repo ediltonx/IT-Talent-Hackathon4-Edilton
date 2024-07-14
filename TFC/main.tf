51f78257c1f9ebf7c735cf350cd0d424dff28dde
edilton.junior <ediltonsilvajr@gmail.com>
2024-07-14 09:08:40 -0300

    corrigindo configuraçoes terraform

--- TFC/main.tf
+++ TFC/main.tf
@@ -1,12 +1,15 @@
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
+  
   tags = {
     Name = "Hacka4"
   }
 }
+
--- TFC/variables.tf
+++ TFC/variables.tf
@@ -4,13 +4,30 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "ami_id" {
-  description = "AMI utilizado da AWS"
-  type        = string
-}
 
 variable "instance_type" {
   description = "Instancia tipo t2.micro"
   type        = string
   default     = "t2.micro"
 }
+
+
+variable "aws_access_key" {
+  description = "Identificador da chave de acesso"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "Identificador da secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "ami_id" {
+  description = "Identificador da AMI"
+  type        = string
+  default     = "ami-06c68f701d8090592"
+}
+
+
