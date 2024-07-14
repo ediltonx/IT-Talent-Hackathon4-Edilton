a0ecb716492e1d53c472b34d940dc5c77129107c
edilton.junior <ediltonsilvajr@gmail.com>
2024-07-14 09:13:42 -0300

    adicionada subrede e vpc

--- TFC/main.tf
+++ TFC/main.tf
@@ -4,12 +4,52 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "VPC-Hacka4"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Subnet-Hacka4"
+  }
+}
+
+resource "aws_security_group" "my_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Security-Hacka4"
+  }
+}
+
 resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  
+  subnet_id     = aws_subnet.my_subnet.id
+  security_groups = [aws_security_group.my_sg.name]
+
   tags = {
     Name = "Hacka4"
   }
 }
-
--- TFC/variables.tf
+++ TFC/variables.tf
@@ -4,7 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-
 variable "instance_type" {
   description = "Instancia tipo t2.micro"
   type        = string
@@ -30,4 +29,14 @@ variable "ami_id" {
   default     = "ami-06c68f701d8090592"
 }
 
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
+variable "subnet_cidr" {
+  description = "The CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
