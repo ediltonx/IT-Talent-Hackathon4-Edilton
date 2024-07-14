bd1fa93d4510abd5feeddd9261794eaa8e6b2868
edilton.junior <ediltonsilvajr@gmail.com>
2024-07-14 18:46:15 -0300

    mudando saidas

--- TFC/outputs.tf
+++ TFC/outputs.tf
@@ -1,12 +1,3 @@
-output "instance_id" {
-  description = "Id gerado da instancia EC2"
-  value       = aws_instance.example.id
-}
-
-output "public_ip" {
-  description = "ip publico da instancia"
-  value       = aws_instance.example.public_ip
-}
 
 output "instance_ami" {
     value = "aws_instance.web.ami"
